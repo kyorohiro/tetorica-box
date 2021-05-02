c7105270a30a37b529d6c9b1b503973903505171
kyorohiro <kyorohiro@gmail.com>
2021-05-02 22:31:13 +0900

    ..

--- web/tetorica-box/src/index.tsx
+++ web/tetorica-box/src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import './style.css'
+//import './style.css'
 
-const Hello = React.createElement('p',{},'Hello React!!')
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+//import 'bootstrap/dist/js/popper.min.js';
+//global.jQuery = require('jquery');
+
+(global as any).jQurey = require('jquery');
+const Hello = React.createElement('h4', { className: 'alert alert-primary' }, 'Hello React!!')
 const rootElement = document.getElementById("root")
 ReactDom.render(Hello, rootElement)
 
