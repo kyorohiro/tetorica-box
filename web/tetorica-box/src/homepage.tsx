36ae4ed8436277b62a23b657f538375cdb2c3018
kyorohiro <kyorohiro@gmail.com>
2021-05-08 03:43:09 +0900

    ..

---
+++ web/tetorica-box/src/homepage.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+import firebase from 'firebase';
+
+function MyHome() {
+    var [email, setEmail] = React.useState("")
+    var [pass, setPass] = React.useState("")
+
+
+    const doClick = async function () {
+        console.log('click')
+        
+        try {
+            firebase.auth().signOut()
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    return (
+        <div>
+            <h1>MyHome</h1>
+            <input type="button" value="Loout" onClick={doClick}></input>
+        </div>
+    )
+}
+
+function MyLogin() {
+    var [email, setEmail] = React.useState("")
+    var [pass, setPass] = React.useState("")
+
+
+    const doClick = async function () {
+        console.log('click')
+        
+        try {
+            var userCredential: firebase.auth.UserCredential = await firebase.auth().signInWithEmailAndPassword(email, pass)
+            console.log(userCredential)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const doChangeEmail = function (props: React.FormEvent<HTMLInputElement>) {
+        console.log(props.currentTarget.value)
+        setEmail(props.currentTarget.value)
+    }
+
+    const doChangePass = function (props: React.FormEvent<HTMLInputElement>) {
+        console.log(props.currentTarget.value)
+        setPass(props.currentTarget.value)
+    }
+
+    return (
+        <div>
+            <h1>MyLogin</h1>
+            <div>{email},{pass}</div>
+            <input type="text" placeholder="Email" onChange={doChangeEmail}></input>
+            <input type="password" placeholder="Password" onChange={doChangePass}></input>
+            <input type="button" value="Login" onClick={doClick}></input>
+        </div>
+    )
+}
+export {MyHome, MyLogin}
+
+/*
+           <input type="text" > </input>
+           */
--- web/tetorica-box/src/index.tsx
+++ web/tetorica-box/src/index.tsx
@@ -1,15 +1,92 @@
-import React from 'react'
+import React, { Children, useContext, useState } from 'react'
 import ReactDom from 'react-dom'
-//import './style.css'
-
+import { Link, BrowserRouter, Router, Route, Switch } from "react-router-dom";
+import { MyHome, MyLogin } from './homepage'
+import firebase from 'firebase';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-//import 'bootstrap/dist/js/popper.min.js';
-//global.jQuery = require('jquery');
-
 (global as any).jQurey = require('jquery');
-const Hello = React.createElement('h4', { className: 'alert alert-primary' }, 'Hello React!!')
+
+
+//
+// initialize firebase
+//
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBme8G9HsEJvYoMr5zkn55GegE_o1ZF9mE",
+    authDomain: "tetorica-box.firebaseapp.com",
+    projectId: "tetorica-box",
+    storageBucket: "tetorica-box.appspot.com",
+    messagingSenderId: "9679189465",
+    appId: "1:9679189465:web:ff0cf134f777abadb715c8",
+    measurementId: "G-1J5QZZ3CH6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+interface AppContextType {
+    logined: boolean,
+}
+
+var AppContext = React.createContext<AppContextType>({
+    logined: false
+})
+
+
+function MyApp(props: any) {
+    // setup context
+    var appContext = useContext(AppContext)
+
+    const [user, setUser] = React.useState<firebase.User>(firebase.auth().currentUser)
+    const [logined, setLogined] = React.useState<boolean>(user ? true : false)
+    var appContext = React.useContext(AppContext)
+    appContext.logined = logined
+    firebase.auth().onAuthStateChanged((u: firebase.User) => {
+        console.log("auth change " + (u ? true : false))
+        appContext.logined = u ? true : false
+        setUser(u)
+        setLogined(u ? true : false)
+    })
+
+    return (
+        <div>
+            <div>
+                <BrowserRouter>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/page'>About</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path='/page' component={MyPage} />
+
+                        <Route path='/' component={logined ? MyHome : MyLogin} />
+                    </Switch>
+                </BrowserRouter>
+                <br />
+                <hr />
+            </div>
+        </div>
+    )
+}
+function MyPage() {
+    return (
+        <div>MyPage 21</div>
+    )
+}
+
+
+
 const rootElement = document.getElementById("root")
-ReactDom.render(Hello, rootElement)
 
-
+let el = (
+    <div>
+        <MyApp />
+    </div>
+)
+ReactDom.render(el, rootElement)
