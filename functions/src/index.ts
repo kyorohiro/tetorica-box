4ee35b20d7f86d901b9d20555cf72d8ece8a0030
kyorohiro <kyorohiro@gmail.com>
2021-05-26 19:11:16 +0900

    t12

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,15 +2,15 @@
 import * as express from "express";
 import * as functions from "firebase-functions";
 
-const PORT = 3000;
+// const PORT = 3000;
 const app = express();
 
 app.get("/", function(req, res) {
   res.send("Admin Homepage");
 });
 
-app.listen(PORT, () => {
-  console.info("server is running on PORT:", PORT);
-});
+// app.listen(PORT, () => {
+//   console.info("server is running on PORT:", PORT);
+// });
 
 exports.app = functions.https.onRequest(app);
